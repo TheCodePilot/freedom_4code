b95bedd5ca924a71cd255e088fc151276b860084
TheCodePilot <116466353+TheCodePilot@users.noreply.github.com>
2025-02-17 07:56:58 +0100

    Update Examples.cpp

--- HWND/Examples.cpp
+++ HWND/Examples.cpp
@@ -15,8 +15,28 @@ int _tmain(int argc, _TCHAR* argv[])
         Application->MainForm->Handle,   // Handle des Fensters
         L"MeinText",                     // Nachrichtentext
         L"MeineÜberschrift",             // Überschrift der Nachricht
-        MB_OK | MB_ICONINFORMATION       // OK-Button und Hinweis-Icon
+        MB_OK | MB_ICONINFORMATION       // OK-Button und Hinweis-Icon (1)
     );
 
     return 0;
 }
+
+
+//=========================================================
+/*
+MB_ICONERROR: Zeigt ein Fehler-Icon (rotes Kreuz).
+
+MB_ICONHAND: Zeigt ebenfalls ein Fehler-Icon (identisch mit MB_ICONERROR).
+
+MB_ICONSTOP: Zeigt ebenfalls ein Fehler-Icon (identisch mit MB_ICONERROR).
+
+MB_ICONQUESTION: Zeigt ein Fragezeichen-Icon.
+
+MB_ICONWARNING: Zeigt ein Warn-Icon (gelbes Dreieck mit Ausrufezeichen).
+
+MB_ICONEXCLAMATION: Zeigt ebenfalls ein Warn-Icon (identisch mit MB_ICONWARNING).
+
+MB_ICONINFORMATION: Zeigt ein Informations-Icon (blaues "i").
+
+MB_ICONASTERISK: Zeigt ebenfalls ein Informations-Icon (identisch mit MB_ICONINFORMATION).
+*/    
